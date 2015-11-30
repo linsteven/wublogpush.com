ff6f4528ff07b86a3ccbcb7461826c527c2fe715
yinyuanlin <yinyuanlin123@gmail.com>
2015-11-30 20:02:42 +0800

    add 404 500 pages

--- wublogpushcom.py
+++ wublogpushcom.py
@@ -85,5 +85,14 @@ def about():
     #return url_for('about')
     return render_template('about.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+  return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", debug=True)
